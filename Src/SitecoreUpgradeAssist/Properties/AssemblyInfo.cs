f8bbbd45690e6dbffeb13879a22fea021b84dc0f
Masoud Ahmadi (Valtech) <masoud.ahmadi@valtech.com>
2020-11-14 17:27:54 +0100

    - more renaming

--- Src/SitecoreUpgradeAssist/Properties/AssemblyInfo.cs
+++ Src/SitecoreUpgradeAssist/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("SitecoreUpgradeAssist")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Wheelbarrowex")]
+[assembly: AssemblyCompany("WheelbarrowEx")]
 [assembly: AssemblyProduct("SitecoreUpgradeAssist")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
